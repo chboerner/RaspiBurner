c313ccb7c1ce981bc57105b723038d7f8f3aa4d8
chboerner <loci@locimotive.de>
2020-12-27 00:40:55 +0100

    First create checkout dir and then use runuser to clone github repo

--- files/base_provision.sh
+++ files/base_provision.sh
@@ -4,10 +4,10 @@ set -e
 source /.provision.conf
 
 echo "Cloning Git repository $github_repo to $provision_basedir"
-git clone --branch master "${github_repo}" "${provision_basedir}"
-echo "Adjusting permissions of clone"
+mkdir -p "${provision_basedir}"
 provision_group=$(getent group $(id -g ${provision_user}) | cut -d ':' -f 1)
 chown -R ${provision_user}:${provision_group} "${provision_basedir}"
+runuser -u ${provision_user} -- git clone --branch master "${github_repo}" "${provision_basedir}"
 
 echo "Checking if Ansible is installed"
 if [ $(which ansible-playbook | wc -l) -eq 0 ]; then
